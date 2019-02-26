b766662112cdc68e82fec57dbccfda7a4d8c8483
kaiskallell <kaiskallel2016@gmail.com>
2019-02-26 15:05:00 +0100

    version0

---
+++ buildroot/package/zbar/zbar.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# zbar
+#
+################################################################################
+
+ZBAR_VERSION = 57d601e82089f2f31de9e1683c3834f237421f5d
+ZBAR_SITE = git://linuxtv.org/zbar.git
+ZBAR_LICENSE = LGPL-2.1+
+ZBAR_LICENSE_FILES = LICENSE
+ZBAR_INSTALL_STAGING = YES
+ZBAR_AUTORECONF = YES
+ZBAR_DEPENDENCIES = libv4l jpeg
+# add host-gettext for AM_ICONV macro
+ZBAR_DEPENDENCIES += host-gettext
+ZBAR_CONF_OPTS = \
+	--disable-doc \
+	--without-imagemagick \
+	--without-qt \
+	--without-qt5 \
+	--without-gtk \
+	--without-python2 \
+	--without-x \
+	--without-java
+
+$(eval $(autotools-package))
